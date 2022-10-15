e6a1ad6f1663d527d2930f7997adcd1bfb3c1c4c
Mohamad Nassar <mnassar57@gmail.com>
2022-10-15 06:17:22 +0300

    frontend: added input and error handling

--- frontend/src/Register.js
+++ frontend/src/Register.js
@@ -4,7 +4,8 @@ const EMAIL_REGEX = /.+@.+\..+/;
 
 const Register = () => {
 
-    const userRef = useRef();
+    const fnameRef = useRef();
+    const lnameRef = useRef();
     const errorRef = useRef();
 
     const [fname, setFname] = useState('');
@@ -20,8 +21,10 @@ const Register = () => {
     const [emailFocus, setEmailFocus] = useState(false);
     const [validEmail, setValidEmail] = useState(false);
 
+    const [errorMessage, setErrorMessage] = useState('');
+
     useEffect(() => {
-        userRef.current.focus();
+        fnameRef.current.focus();
     }, []);
 
     useEffect(() => {
@@ -30,9 +33,32 @@ const Register = () => {
         setValidEmail(result);
     }, [email]);
 
+    useEffect(() => {
+        setErrorMessage('')
+    }, [fname, lname, email, password])
+
     return(
         <>
-            Register, gg
+            {errorMessage ?? <p ref={errorRef}>{errorMessage}</p>}
+            <h1>Register</h1>
+            <form>
+                <label htmlFor="fname">
+                    First Name:
+                </label>
+                <input
+                    type="text"
+                    id="fname"
+                    ref={fnameRef}
+                    autoComplete="off"
+                    onChange={(e) => setFname(e.target.value)}
+                    required
+                    aria-invalid={fname.length > 2 ? true : false}
+                    aria-describedby="uidnote"
+                    onFocus={() => {setFnameFocus(true)}}
+                    onBlur={() => {setFnameFocus(false)}}
+                />
+                {fname.length < 3 && fname && <p id="uidnote">First name should be larger than 3 characters</p>}
+            </form>
         </>
     )
 }
