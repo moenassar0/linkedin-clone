171b580ea88f31d038b4efa6542787c9733d97c3
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 02:43:44 +0300

    frontend: dummy jobs feed page added

---
+++ frontend/src/Components/User/Feed.js
@@ -0,0 +1,64 @@
+export const Feed = () => {
+
+    return(
+        <>
+            <div className="feed-container">
+                <div className="jobs-feed-container">
+                    <div className="jobs-feed-header">Jobs in Lebanon</div>
+                    <div className="jobs-card-container">
+                        <div className="jobs-card">
+                            <div className="jobs-card-img">
+                                <img src="../../images/test.png" className="img-resize"></img>
+                            </div>
+                            <div className="jobs-card-info">
+                                Junior Web Developer
+                            </div>
+                        </div>
+                        <div className="jobs-card">
+                            <div className="jobs-card-img">
+                                <img src="../../images/test.png" className="img-resize"></img>
+                            </div>
+                            <div className="jobs-card-info">
+                                Junior Web Developer
+                            </div>
+                        </div>
+                        <div className="jobs-card">
+                            <div className="jobs-card-img">
+                                <img src="../../images/test.png" className="img-resize"></img>
+                            </div>
+                            <div className="jobs-card-info">
+                                Junior Web Developer
+                            </div>
+                        </div>
+                        <div className="jobs-card">
+                            <div className="jobs-card-img">
+                                <img src="../../images/test.png" className="img-resize"></img>
+                            </div>
+                            <div className="jobs-card-info">
+                                Junior Web Developer
+                            </div>
+                        </div>
+                                                <div className="jobs-card">
+                            <div className="jobs-card-img">
+                                <img src="../../images/test.png" className="img-resize"></img>
+                            </div>
+                            <div className="jobs-card-info">
+                                Junior Web Developer
+                            </div>
+                        </div>
+                        <div className="jobs-card">
+                            <div className="jobs-card-img">
+                                <img src="../../images/test.png" className="img-resize"></img>
+                            </div>
+                            <div className="jobs-card-info">
+                                Junior Web Developer
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Feed;
