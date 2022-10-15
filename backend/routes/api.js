5f24c759bf3f8d865f8639c028972520b1c68a87
Mohamad Nassar <mnassar57@gmail.com>
2022-10-15 09:04:16 +0300

    backend: updated routes

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,9 @@
 const { Router } = require('express');
 const router = Router();
-const { createUser } = require('../controllers/UserController');
+const { createUser, login } = require('../controllers/UserController');
 
 
 router.post('/users', createUser);
+router.post('/login', login);
 
 module.exports = router;
