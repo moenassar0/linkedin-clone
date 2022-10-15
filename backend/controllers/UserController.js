f0102f8584a74b5619bdb0e14dbdb0d2853b7de4
Mohamad Nassar <mnassar57@gmail.com>
2022-10-15 21:25:39 +0300

    backend: updated register function

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,10 +1,32 @@
 const UserModel = require('../models/User');
 
 const createUser = async (req, res) => {
+    //Check if email is taken
+    /*UserModel.find({ email: req.body.email }).exec((user) => {
+        if(user.length)
+        {
+            res.status(409).send("Email taken");
+            console.log("found");
+            return
+        }
+        else{
+            console.log("gg");
+        };
+    })
+    
+
     UserModel.create(req.body)
     .then((user) => res.send("User created:" + user))
-    .catch(err => res.status(400).send('Error: ' + err))
-    
+    .catch(err => res.status(400).send('Error: ' + err))*/
+
+    try {
+        insertResult = await UserModel.create(req.body)
+        res.send("User created:" + insertResult)
+    }
+    //Check if email is taken
+    catch(err) {
+        res.status(409).send('Taken email')
+    }
 }
 
 const login = async (req, res) => {
