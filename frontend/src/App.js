6879f32796813130fbd2297b3f26718f06475335
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 00:02:06 +0300

    frontend: added routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
 import './App.css';
+import EditProfile from './Components/Popups/EditProfile';
 import Register from './Register';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  useRoutes,
+  BrowserRouter,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <Register />
+      <Routes>
+        <Route path="/" element={<Register />}></Route>
+        <Route path="/user" element={ <EditProfile /> }>
+          <Route path="/user/profile" element={<EditProfile />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
