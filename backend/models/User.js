0ce0c991cc750b0b8198e3b12a67b021ed1a11fb
Mohamad Nassar <mnassar57@gmail.com>
2022-10-15 08:08:49 +0300

    backend: updated user model

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,7 +1,26 @@
 const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
-
+    fname: {
+        type: String,
+        required: "First name is required!"
+    },
+    lname: {
+        type: String,
+        required: "Last name is required!"
+    },
+    email: {
+        type: String,
+        required: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+    }
 });
 
 const model = mongoose.model('User', userSchema);
