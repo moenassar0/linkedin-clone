756cdc8284e3d7850be889bc60bfdbc6209a0fed
Mohamad Nassar <mnassar57@gmail.com>
2022-10-15 23:39:51 +0300

    backend: updated login function for jwt token

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,4 +1,5 @@
 const UserModel = require('../models/User');
+const jwt = require('jsonwebtoken');
 
 const createUser = async (req, res) => {
     //Check if email is taken
@@ -30,9 +31,27 @@ const createUser = async (req, res) => {
 }
 
 const login = async (req, res) => {
-    UserModel.find({ email: req.body.email, password: req.body.password}).exec()
+
+    try {
+        user = await UserModel.find({ email: req.body.email, password: req.body.password}).select("+password")
+        if(user.length){
+            console.log((JSON.stringify(user)))
+            const access_token = jwt.sign(JSON.stringify(user), process.env.JWT_SECRET)
+            //res.json({access_token: access_token})
+            console.log("gg");
+            res.send(access_token);
+        }
+        
+        else
+            res.send("User not found!");
+    }
+    catch(err) {
+        res.status(400).send('Server error: ' + err)
+    }
+
+    /*UserModel.find({ email: req.body.email, password: req.body.password}).exec()
     .then((user) => res.send("User found:" + user))
-    .catch(err => res.status(400).send('Error: ' + err))
+    .catch(err => res.status(400).send('Error: ' + err))*/
 }
 
 module.exports = {
