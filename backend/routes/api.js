241d52723ace1f5900e0268409ae7884cd63dea4
Mohamad Nassar <mnassar57@gmail.com>
2022-10-15 23:40:09 +0300

    backend: added tester middleware function

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,9 +1,27 @@
 const { Router } = require('express');
+const jwt = require('jsonwebtoken');
 const router = Router();
 const { createUser, login } = require('../controllers/UserController');
 
 
 router.post('/users', createUser);
 router.post('/login', login);
+router.post('/test', authenticateToken, (req, res) => {
+    res.status(200).send("gg worked: " + JSON.stringify(req.user));
+})
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(" ")[1];
+    if(token == null) return res.sendStatus(401);
+
+    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+        if(err){
+            return res.sendStatus(403);
+        }
+        req.user = user;
+        next();
+    })
+}
 
 module.exports = router;
