46a45b604ceb0c7731dbc310bbc30b31a7191ab4
Mohamad Nassar <mnassar57@gmail.com>
2022-10-17 02:26:48 +0300

    frontend: added unfollow/follow functions

--- frontend/src/Components/User/CompanyFeed.js
+++ frontend/src/Components/User/CompanyFeed.js
@@ -3,19 +3,28 @@ import axios from "../../api/axios";
 export const CompanyFeed = () => {
 
     const [companies, setCompanies] = useState([]);
+    const [user, setUser] = useState([]);
+    const headers = {headers:{'Authorization' : "Bearer " + localStorage.getItem("token")}};
 
     useEffect(() => {
         fetchCompanies()
     }, [])
 
     async function fetchCompanies() {
-        const response = await axios.get('/companies');
+        const response = await axios.get('/companies', headers);
         console.log(response.data)
         setCompanies(response.data);
     }
 
-    function followCompany(company_id){
-        console.log(company_id);
+    async function followCompany(company_id){
+        const response = await axios.post('/follow', {company_id}, headers);
+        fetchCompanies();
+    }
+
+    async function unfollowCompany(company_id){
+        const response = await axios.post('/unfollow', {company_id}, headers);
+        console.log(response);
+        fetchCompanies();
     }
 
     return(
@@ -30,7 +39,11 @@ export const CompanyFeed = () => {
                             <span className="bold">{company.company_title}</span>
                             <span>Software Development</span>
                             <span className="smaller-font grey">19,000 Followers</span>
-                            <button onClick={() => {followCompany(company._id)}} className="follow-btn">+ Follow</button>
+                            {company.following 
+                            ? 
+                            <button onClick={() => {unfollowCompany(company._id)}} className="follow-btn">UnFollow</button>
+                            : 
+                            <button onClick={() => {followCompany(company._id)}} className="follow-btn">+ Follow</button>}
                         </div>
                     </div>
                 ))}
