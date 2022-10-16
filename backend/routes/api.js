b6ace4c54067ddc851be777e05a1baa03e2ab6de
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 03:20:45 +0300

    backend: added base64 to image function

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,5 +1,6 @@
 const { Router } = require('express');
 const jwt = require('jsonwebtoken');
+const fs = require('fs');
 const router = Router();
 const { createUser, login } = require('../controllers/UserController');
 
@@ -8,6 +9,13 @@ router.post('/users', createUser);
 router.post('/login', login);
 router.post('/test', authenticateToken, (req, res) => {
     res.status(200).send("gg worked: " + JSON.stringify(req.user));
+})
+
+//Tester
+router.post('/image', (req, res) => {
+    let data = req.body.data;
+    let buff = Buffer.from(data, 'base64');
+    fs.writeFileSync('stack-abuse-logo-out.png', buff);
 })
 
 function authenticateToken(req, res, next){
