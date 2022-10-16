e2680c611af8d580f861bc8183480b951ed8abcb
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 07:31:21 +0300

    frontend: fetched data for feed page

--- frontend/src/Components/User/Feed.js
+++ frontend/src/Components/User/Feed.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+import axios from "../../api/axios";
 export const Feed = () => {
+
+    const [jobOfferings, setJobOfferings] = useState([]);
+
+    useEffect(() => {
+        fetchJobOfferings()
+    }, [])
+
+    async function fetchJobOfferings() {
+        const response = await axios.get('/jobofferings');
+        console.log(response.data)
+        setJobOfferings(response.data);
+    }
 
     return(
         <>
@@ -6,7 +20,11 @@ export const Feed = () => {
                 <div className="jobs-feed-container">
                     <div className="jobs-feed-header">Jobs in Lebanon</div>
                     <div className="jobs-card-container">
-                    
+                    {jobOfferings.map((item, i) => (
+                                    <div key={i}>
+                                        {item.job_title}
+                                    </div>
+                                ))}
                     </div>
                 </div>
             </div>
