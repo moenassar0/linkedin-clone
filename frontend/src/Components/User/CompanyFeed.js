5ecf47ac8b8e49f328194d303f35ca8b29a68011
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 20:57:14 +0300

    frontend: added company card

--- frontend/src/Components/User/CompanyFeed.js
+++ frontend/src/Components/User/CompanyFeed.js
@@ -5,7 +5,14 @@ export const CompanyFeed = () => {
     return(
         <>
             <div className="feed-container">
-
+                <div className="company-card-container">
+                    <div className="company-card-img">
+                        <img className="img-resize" src="../../images/linkedin_icon.png"></img>
+                    </div>
+                    <div className="company-card-info">
+                        <span className="bold">CobbleStone Energy</span>
+                    </div>
+                </div>
             </div>
         </>
     )
