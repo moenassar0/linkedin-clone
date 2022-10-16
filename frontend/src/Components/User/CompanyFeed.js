aa3d5b31e08a7182781f1706fdc1416771817538
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 22:44:21 +0300

    frontend: fetched companies from db

--- frontend/src/Components/User/CompanyFeed.js
+++ frontend/src/Components/User/CompanyFeed.js
@@ -2,17 +2,34 @@ import { useState, useEffect } from "react";
 import axios from "../../api/axios";
 export const CompanyFeed = () => {
 
+    const [companies, setCompanies] = useState([]);
+
+    useEffect(() => {
+        fetchCompanies()
+    }, [])
+
+    async function fetchCompanies() {
+        const response = await axios.get('/companies');
+        console.log(response.data)
+        setCompanies(response.data);
+    }
+
     return(
         <>
             <div className="feed-container">
-                <div className="company-card-container">
+                {companies.map((company, i) => (
+                    <div className="company-card-container">
                     <div className="company-card-img">
                         <img className="img-resize" src="../../images/linkedin_icon.png"></img>
                     </div>
                     <div className="company-card-info">
-                        <span className="bold">CobbleStone Energy</span>
+                        <span className="bold">{company.company_title}</span>
+                        <span>Software Development</span>
+                        <span className="smaller-font grey">19,000 Followers</span>
+                        <button className="follow-btn">+ Follow</button>
                     </div>
                 </div>
+                ))}
             </div>
         </>
     )
