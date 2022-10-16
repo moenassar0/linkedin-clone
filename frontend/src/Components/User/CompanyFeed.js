ce0dbf4e28261ce6d5ea07a81c05ebac1e73b09b
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 20:49:06 +0300

    frontend: added starter companyfeed page

---
+++ frontend/src/Components/User/CompanyFeed.js
@@ -0,0 +1,14 @@
+import { useState, useEffect } from "react";
+import axios from "../../api/axios";
+export const CompanyFeed = () => {
+
+    return(
+        <>
+            <div className="feed-container">
+
+            </div>
+        </>
+    )
+}
+
+export default CompanyFeed;
