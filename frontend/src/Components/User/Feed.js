66951f6b94c523e0400bac74499f93d6ee0a946c
Mohamad Nassar <mnassar57@gmail.com>
2022-10-16 09:25:29 +0300

    frontend: updated feed

--- frontend/src/Components/User/Feed.js
+++ frontend/src/Components/User/Feed.js
@@ -3,6 +3,7 @@ import axios from "../../api/axios";
 export const Feed = () => {
 
     const [jobOfferings, setJobOfferings] = useState([]);
+    const [currentJobOffer, setCurrentJobOffer] = useState([]);
 
     useEffect(() => {
         fetchJobOfferings()
@@ -14,18 +15,34 @@ export const Feed = () => {
         setJobOfferings(response.data);
     }
 
+    function setJob(jobOffering){
+        setCurrentJobOffer(jobOffering);
+        console.log(jobOffering._id);
+    }
+
     return(
         <>
             <div className="feed-container">
                 <div className="jobs-feed-container">
                     <div className="jobs-feed-header">Jobs in Lebanon</div>
                     <div className="jobs-card-container">
+
                     {jobOfferings.map((item, i) => (
-                                    <div key={i}>
-                                        {item.job_title}
-                                    </div>
+                        <div key={i} className="jobs-card">
+                        <div className="jobs-card-img">
+                            <img className='img-resize' src="../../images/linkedin_icon.png"></img>
+                        </div>
+                        <div className="jobs-card-info">
+                            <span className="bold blue pointer" onClick={() => {setJob(item)}}>{item.job_title}</span>
+                            <span>{item.assoc_company?.company_title}</span>
+                            <span className="grey">{item.location}</span>
+                        </div>
+                    </div>
                                 ))}
                     </div>
+                </div>
+                <div className="job-expanded">
+                {currentJobOffer.length == 0 ? "Click on one of the job offers" : currentJobOffer.job_title}
                 </div>
             </div>
         </>
