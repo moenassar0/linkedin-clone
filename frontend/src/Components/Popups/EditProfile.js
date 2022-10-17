3d0c805c7f4ebe1e6e14d231af2b221794b2ad72
Mohamad Nassar <mnassar57@gmail.com>
2022-10-17 06:08:31 +0300

    frontend: edit profile button updated

--- frontend/src/Components/Popups/EditProfile.js
+++ frontend/src/Components/Popups/EditProfile.js
@@ -1,12 +1,20 @@
-export const EditProfile = () => {
+import { useState } from "react";
 
-    return(
+export const EditProfile = (props) => {
+
+    const [fname, setFname] = useState('');
+    const [validFName, setValidFName] = useState(false);
+
+    const [lname, setLname] = useState('');
+    const [validLName, setValidLName] = useState(false);
+
+    return((props.trigger) ?
         <>
             <div className="popup-cover">
                 <div className="popup-form">
                     <div className="popup-header">
-                        <h3>Edit Profile</h3>
-                        <button className="close-btn pointer">X</button>
+                        <h3>Edit Profile for {auth.user}</h3>
+                        <button onClick={() => {props.setTrigger(false)}} className="close-btn pointer">X</button>
                     </div>
                     <div className="register-field padding-15">
                         <label htmlFor="fname">
@@ -18,11 +26,42 @@ export const EditProfile = () => {
                             autoComplete="off"
                             aria-describedby="uidnote"
                         />
-                        
+                    </div>
+                    <div className="register-field padding-15">
+                        <label htmlFor="fname">
+                            Location:
+                        </label>
+                        <input
+                            type="text"
+                            id="fname"
+                            autoComplete="off"
+                            aria-describedby="uidnote"
+                        />
+                    </div>
+                    <div className="register-field padding-15">
+                        <label htmlFor="status">
+                            Employment Status:
+                        </label>
+                        <select id="status" onChange={(e) => setInstructor(e.target.value)}>
+                            <option value="student">Student</option>
+                            <option value="employed">Employed</option>
+                            <option value="unemployed">Unemployed</option>
+                        </select>
+                    </div>
+                    <div className="register-field padding-15">
+                        <label htmlFor="fname">
+                            First Name:
+                        </label>
+                        <input
+                            type="text"
+                            id="fname"
+                            autoComplete="off"
+                            aria-describedby="uidnote"
+                        />
                     </div>
                 </div>
             </div>
-        </>
+        </> : ""
     )
 }
 
