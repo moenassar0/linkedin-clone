b451f3f493c24b8fb02bd26ad6a85adad0a83762
Mohamad Nassar <mnassar57@gmail.com>
2022-10-17 06:25:54 +0300

    frontend: added edit profile popup

--- frontend/src/Components/User/Profile.js
+++ frontend/src/Components/User/Profile.js
@@ -1,29 +1,37 @@
 import React from 'react'
+import { useState } from 'react';
+import EditProfile from '../Popups/EditProfile';
 
 const Profile = () => {
+
+    const [editProfileButton, setEditProfileButton] = useState(false);
+    function editProfile(){
+        setEditProfileButton(true);
+    }
   return (
-    <div className='main-container'>
-        <div className='profile-container'>
-            <div className='profile-cover-container'>
-                <div className='profile-cover'>
-                <div className='profile-image'>
-                    <img className='img-resize circle' src="../../images/linkedin_icon.png"></img>
+    <>
+        <EditProfile trigger={editProfileButton} setTrigger={setEditProfileButton} />
+        <div className='main-container'>
+            <div className='profile-container'>
+                <div className='profile-cover-container'>
+                    <div className='profile-cover'>
+                    <div className='profile-image'>
+                        <img className='img-resize circle' src="../../images/linkedin_icon.png"></img>
+                    </div>
+                    </div>
                 </div>
+                <div className='profile-info'>
+                    <div className="profile-name-edit">
+                        <span className='bold'>Mohamad Nassar</span>
+                        <button onClick={() => {editProfile()}}>Edit</button>
+                    </div>
+                    <span className='profile-employment'>Student</span>
+                    <span className='profile-location grey'>Beirut, Lebanon</span>
+                    <button className='pointer'>Open CV</button>
                 </div>
-
-            </div>
-
-            <div className='profile-info'>
-                <div className="profile-name-edit">
-                    <span className='bold'>Mohamad Nassar</span>
-                    <button>Edit</button>
-                </div>
-                <span className='profile-employment'>Student</span>
-                <span className='profile-location grey'>Beirut, Lebanon</span>
-                <button className='pointer'>Open CV</button>
             </div>
         </div>
-    </div>
+    </>
   )
 }
 
