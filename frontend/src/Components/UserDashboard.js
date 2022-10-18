4262f86192fabfa7385d5abd4ea20a80abb6ba3e
Mohamad Nassar <mnassar57@gmail.com>
2022-10-18 07:36:15 +0300

    frontend: updated dashboard

--- frontend/src/Components/UserDashboard.js
+++ frontend/src/Components/UserDashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import UserNavbar from './UserNavbar'
 import { BrowserRouter as Router, Link, Routes, Route, Outlet, Navigate } from 'react-router-dom';
+import Feed from './User/Feed';
 
 const UserDashboard = () => {
   return (
