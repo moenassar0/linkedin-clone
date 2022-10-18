1ccb2fa84e3c6cbb07a7ed9e0bced77e6a8ae49a
Mohamad Nassar <mnassar57@gmail.com>
2022-10-18 07:36:34 +0300

    frontend: updated navbar

--- frontend/src/Components/CompanyNavbar.js
+++ frontend/src/Components/CompanyNavbar.js
@@ -18,12 +18,12 @@ const CompanyNavbar = () => {
                     </svg>
                     <span>Home</span>
                 </Link>
-                <div className='navbar-item pointer'>
+                <Link className='navbar-item pointer' to="/company/jobs">
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" data-supported-dps="24x24" fill="currentColor" width="24" height="24" focusable="false">
                         <path d="M17 6V5a3 3 0 00-3-3h-4a3 3 0 00-3 3v1H2v4a3 3 0 003 3h14a3 3 0 003-3V6zM9 5a1 1 0 011-1h4a1 1 0 011 1v1H9zm10 9a4 4 0 003-1.38V17a3 3 0 01-3 3H5a3 3 0 01-3-3v-4.38A4 4 0 005 14z"></path>
                     </svg>
                     <span>Jobs</span>
-                </div>
+                </Link>
             </div>
         </div>
     </>
